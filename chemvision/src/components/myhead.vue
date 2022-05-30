560ff275fec221a3a26fa11cff2039992fa62185
lxthnu <58248490+lxthnu@users.noreply.github.com>
2022-05-30 23:22:04 +0800

    Update myhead.vue

--- chemvision/src/components/myhead.vue
+++ chemvision/src/components/myhead.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="height: 50px; line-height: 50px; border-bottom: 1px solid #ccc; display: flex">
-    <div style="text-align: center ;width: 100%; padding-left: 30px; font-weight: bold; color: dodgerblue">五朵金花化学虚拟仿真平台</div>
+    <div style="text-align: center ;width: 100%; padding-left: 30px; font-weight: bold; color: dodgerblue">甲苯仿生催化氧化动力学模拟平台</div>
     <div style="flex: 1"></div>
     <div style="width: 100px">
 
